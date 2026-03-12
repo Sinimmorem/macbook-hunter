fa58938184ba19a20b04104a61b26c0c1bb7dd7b
Sinimmorem <sinimmorem@gmail.com>
2026-03-12 17:39:14 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
-const CACHE = 'mbhunter-v1';
 self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request));
 });
